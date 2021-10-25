a12f5a7d1b404a86653794b0797bcbe7705b8734
yinyue <yinyue@gridsum.com>
2021-10-25 11:06:56 +0800

    chore: add lint

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        'plugin:prettier/recommended'
+    ],
+    "parserOptions": {
+        "ecmaVersion": 13,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {}
+};
